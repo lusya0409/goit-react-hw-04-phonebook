8a8d39f4f834f6d5960c514a27401c4d1847c3eb
lusya0409 <lusya0409@gmail.com>
2023-09-08 19:37:18 +0300

    refactoring the code into react-hooks

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
-export class Phonebook extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+import { useState, useEffect } from 'react';
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('saved-contacts');
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
-    }
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem('saved-contacts');
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
   }
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts)
-      localStorage.setItem(
-        'saved-contacts',
-        JSON.stringify(this.state.contacts)
-      );
-  }
+  return [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+};
 
-  addContact = newContact => {
-    const hasContact = this.state.contacts.find(
+export const Phonebook = () => {
+  const [contacts, setContacts] = useState(getInitialContacts);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = newContact => {
+    const hasContact = contacts.find(
       ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
     );
     if (hasContact) {
@@ -39,42 +34,33 @@ export class Phonebook extends Component {
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
-    }));
+    setContacts(prevContacts => [
+      ...prevContacts,
+      { id: nanoid(), ...newContact },
+    ]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
-
-    return contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(filter.toLowerCase());
-    });
-  };
-  changeFilter = newFilter => {
-    this.setState({
-      filter: newFilter,
-    });
-  };
-
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onAdd={this.addContact} />
-        <h2>Contacts</h2>
-        <ContactList items={filteredContacts} onDelete={this.deleteContact} />
-        <Filter filter={filter} onChangeFilter={this.changeFilter} />
-      </div>
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
-  }
-}
+  };
+  const filteredContacts = contacts.filter(({ name }) => {
+    return name.toLowerCase().includes(filter.toLowerCase());
+  });
+
+  const changeFilter = newFilter => {
+    setFilter(newFilter);
+  };
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <h2>Contacts</h2>
+      {}
+      <ContactList items={filteredContacts} onDelete={deleteContact} />
+      <Filter filter={filter} onChangeFilter={changeFilter} />
+    </div>
+  );
+};
